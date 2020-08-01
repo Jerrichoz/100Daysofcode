fa26f58b61282216fecd72f698645120e569c945
Jerrichoz <maksim.scheierman@gmx.de>
2020-08-01 18:09:01 +0200

    End of coding for Day 4

--- 4DaysofCode.ts
+++ 4DaysofCode.ts
@@ -34,3 +34,51 @@ function isOdd(value: number): boolean{
     return false;
 
 }
+
+// You have a string that consist only of digits. 
+// You need to find how many zero digits ("0") are at the beginning of the given string.
+function beginningZeros(text: string): number {
+    // my code here
+    var zeroCounter = 0;
+    for (let index = 0; index < text.length; index++) {
+        const element = text[index];
+        if(element === "0")
+        {
+            zeroCounter++;
+        }
+        else
+        {
+            return zeroCounter;
+        }
+        
+    }
+    return zeroCounter;
+}
+
+// Find the nearest value to the given one.
+// You are given a list of values as Array form and a value for which you need to find the nearest one.
+// For example, we have the following set of numbers: 4, 7, 10, 11, 12, 17, 
+// and we need to find the nearest value to the number 9. If we sort this set in the ascending order, 
+// then to the left of number 9 will be number 7 and to the right - will be number 10. But 10 is closer than 7, 
+// which means that the correct answer is 10.
+
+function nearestValue(values: number[], search: number): number {
+    // my code here
+    sortArrayInt(values);
+    var lowestNumber: number = values[0];
+    values.forEach(element => {
+        if(Math.abs(element-search) < Math.abs(lowestNumber-search))
+        {
+            lowestNumber = element;
+
+        }
+    });
+    return lowestNumber;
+}
+
+function sortArrayInt(values: number[]): number[]
+{
+    return values.sort(function(a, b){
+        return a - b;
+    });
+}
