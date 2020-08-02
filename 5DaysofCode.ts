b2eddcddf1f2579615a27c735493215ae1fed27d
Jerrichoz <maksim.scheierman@gmx.de>
2020-08-02 22:23:34 +0200

    new assignment

--- 5DaysofCode.ts
+++ 5DaysofCode.ts
@@ -59,3 +59,13 @@ function isOdd(value: number): boolean{
     console.log("is even!")
     return false;
 }
+
+
+// In a given text you need to sum the numbers. 
+// Only separated numbers should be counted. If a number is part of a word it shouldn't be counted.
+// The text consists from numbers, spaces and english letters
+function sumNumbers(test: string): number {
+    // my code here
+    let resultString = (\ \d+\ )+;
+    return 0;
+}
